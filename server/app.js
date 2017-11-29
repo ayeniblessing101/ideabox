7c898bd4d3dcfbdc9f89a2ba8259781f53e1d71c
Ayeni Blessing <ayeniblessing101@gmail.com>
2017-11-29 17:08:20 +0100

    chore(setup expressjs): setup and test the express development environment

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Running on localhost: ${port}`));
+
+export default app;
