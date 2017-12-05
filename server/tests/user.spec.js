0357a9c3ee570e3b07cb6993d89c4109c60e25aa
Ayeni Blessing <ayeniblessing101@gmail.com>
2017-12-05 17:06:38 +0100

    chore(server side test): add server side test

--- server/tests/user.spec.js
+++ server/tests/user.spec.js
@@ -6,7 +6,7 @@ import users from '../tests/mockData/users.json';
 
 const { expect } = chai;
 const request = supertest(app);
-let jwtToken;
+let jwtToken, Id;
 
 describe('User Controller', () => {
   before((done) => {
@@ -23,6 +23,7 @@ describe('User Controller', () => {
         .send(users[0])
         .expect(201)
         .end((err, res) => {
+          Id = res.body.user.userId;
           jwtToken = res.body.token;
           expect(res.body).to.be.an('object');
           expect(res.body.user).to.be.an('object');
@@ -87,16 +88,29 @@ describe('User Controller', () => {
     });
   });
 
-  describe('when a user signin with an email and password field blank', () => {
-    it('should return email is required, password is required', (done) => {
+  describe('when a user signin with email field blank ', () => {
+    it('should return email is required', (done) => {
       request
-        .post('/api/v1/user/signup')
+        .post('/api/v1/user/login')
         .set('Accept', 'application/x-www-form-urlencoded')
         .send(users[3])
         .expect(400)
         .end((err, res) => {
-          expect(JSON.parse(res.error.text).errors[2].msg).to.equal('email is required', );
-          expect(JSON.parse(res.error.text).errors[3].msg).to.equal('password is required', );
+          expect(JSON.parse(res.error.text).errors[0].msg).to.equal('email is required', );
+          done();
+        });
+    });
+  });
+
+  describe('when a user signin with password field blank ', () => {
+    it('should return password is required', (done) => {
+      request
+        .post('/api/v1/user/login')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send(users[3])
+        .expect(400)
+        .end((err, res) => {
+          expect(JSON.parse(res.error.text).errors[2].msg).to.equal('password is required', );
           done();
         });
     });
@@ -105,12 +119,12 @@ describe('User Controller', () => {
   describe('when a user signin with an invalid email', () => {
     it('should return Invalid email', (done) => {
       request
-        .post('/api/v1/user/signup')
+        .post('/api/v1/user/login')
         .set('Accept', 'application/x-www-form-urlencoded')
         .send(users[2])
         .expect(400)
         .end((err, res) => {
-          expect(JSON.parse(res.error.text).errors[4].msg).to.equal('Invalid email', );
+          expect(res.body.errors[0].msg).to.equal('Invalid email');
           done();
         });
     });
@@ -169,6 +183,48 @@ describe('User Controller', () => {
         });
     });
   });
+
+  describe('when a user update their profile with a firstname, lastname and email field blank', () => {
+    it('should return a firstname, lastname and email is required', (done) => {
+      request
+        .put(`/api/v1/user/${Id}`)
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .set('x-access-token', jwtToken)
+        .send({
+          ...users[0],
+          firstname: '',
+          lastname: '',
+          email: '',
+        })
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(JSON.parse(res.error.text).errors[2].msg).to.equal('firstname is required');
+          expect(JSON.parse(res.error.text).errors[3].msg).to.equal('lastname is required');
+          expect(JSON.parse(res.error.text).errors[4].msg).to.equal('email is required');
+          done();
+        });
+    });
+  });
+
+  describe('when a user update their profile with a valid userId', () => {
+    it('should return a  success message `Your profile was updated successfully ', (done) => {
+      request
+        .put(`/api/v1/user/${Id}`)
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .set('x-access-token', jwtToken)
+        .send({
+          ...users[0],
+          firstname: 'Blessing',
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          done();
+        });
+    });
+  });
+
   describe('when a user update their profile with a invalid userId', () => {
     it('should return a  error message `User not Found ', (done) => {
       request
@@ -180,6 +236,142 @@ describe('User Controller', () => {
         .end((err, res) => {
           expect(res.body).to.be.an('object');
           expect(res.body).to.have.a.property('error', 'User not Found');
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to reset password with the email field blank', () => {
+    it('should return a  error message `email is required` ', (done) => {
+      request
+        .post('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send({
+          ...users[0],
+          email: '',
+        })
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(JSON.parse(res.error.text).errors[0].msg).to.equal('email is required');
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to reset password with an Invalid Email', () => {
+    it('should return a  error message `Invalid email` ', (done) => {
+      request
+        .post('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send({
+          ...users[0],
+          email: '@eroi3dd',
+        })
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body.errors[0].msg).to.equal('Invalid email');
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to reset password with an Email that does not exist', () => {
+    it('should return a  error message `Email does not exist` ', (done) => {
+      request
+        .post('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send({
+          email: 'ayeniblessing@gmail.com',
+        })
+        .expect(404)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.a.property('error', 'Email does not exist');
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to reset password with an Email that exist', () => {
+    it('should return a success message `Check your email to continue resetting your password` ', (done) => {
+      request
+        .post('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send({
+          email: 'tomiwa@gmail.com',
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.a.property(
+            'message',
+            'Check your email to continue resetting your password',
+          );
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to save a new password with newPassword field empty', () => {
+    it('should return a  error message `New password is required` ', (done) => {
+      request
+        .put('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send(users[4])
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body.errors[0].msg).to.equal('New password is required');
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to save a new password with confirmNewPassword field empty', () => {
+    it('should return a  error message `confirmNewPassword is required` ', (done) => {
+      request
+        .put('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send(users[5])
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body.errors[1].msg).to.equal('Please confirm password');
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to save a new password and passwords do not match', () => {
+    it('should return a  error message `Password does not match` ', (done) => {
+      request
+        .put('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send(users[6])
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body.errors[0].msg).to.equal('Password does not match');
+          done();
+        });
+    });
+  });
+
+  describe('when a user want to save a new password with an Invalid token', () => {
+    it('should return a error message `Invalid password reset token` ', (done) => {
+      request
+        .put('/api/v1/resetpassword')
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .send(users[7])
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.a.property(
+            'error',
+            'Invalid password reset token',
+          );
           done();
         });
     });
