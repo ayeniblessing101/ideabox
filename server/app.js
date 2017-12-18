ec64d1ad1c6e10227f9af33e8b4c1125b6d37b19
Ayeni Blessing <ayeniblessing101@gmail.com>
2017-12-18 09:31:48 +0100

    chore(client-side-test): add actions test

--- server/app.js
+++ server/app.js
@@ -16,16 +16,20 @@ const compiler = webpack(config);
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, '../api_docs/')));
 app.use('/', express.static(path.join(__dirname, '../dist')));
+app.use('/api', express.static(path.join(__dirname, '../api_docs/')));
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'development') {
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     hot: true,
     publicPath: config.output.publicPath,
   }), );
   app.use(webpackHotMiddleware(compiler));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 }
 
 app.use(bodyParser.json());
@@ -34,12 +38,14 @@ app.use(expressValidator());
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', ideaRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
+
 app.get('/apiDocs', (req, res) => {
   res.sendFile(path.join(__dirname, '../api_docs/index.html'));
-});
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,80 +2,43 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: './client/index',
+  entry: [
+    path.join(__dirname, 'client/index'),
+    path.resolve(__dirname, 'client/styles/style.scss'),
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
     publicPath: '/',
+    filename: 'bundle.min.js',
   },
-  devtool: 'source-map',
-  plugins: [
-    new ExtractTextPlugin('css/bundle.css'),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      },
-    }),
-    new UglifyJsPlugin({
-      sourceMap: true,
-    }),
-    new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      'window.$': 'jquery',
-      'window.jQuery': 'jquery',
-      Hammer: 'hammerjs/hammer',
-    }),
-    new TransferWebpackPlugin([{ from: 'client/images', to: 'images' }]),
-  ],
   module: {
     loaders: [
-      // js
       {
-        test: /\.js$|.jsx$/,
-        use: 'babel-loader',
-        include: path.join(__dirname, 'client'),
-        exclude: /(node_module)/,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-2'],
+        },
       },
-      // css
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loaedr',
-          },
-        ],
-      },
-      // scss
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
+        loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
+          loader: 'css-loader?importLoaders=1',
         }),
       },
-      // fonts
       {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'file-loader',
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
       },
       {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use:
-          'url-loader?limit=10000&mimetype=application/octet-stream&name=./client/fonts/[name].[ext]',
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        test: /\.(eot|svg|ttf|woff|woff2|styl)$/,
+        loader: 'file-loader',
       },
       {
         test: /\.(jpg|jpeg|png|svg)$/,
@@ -84,13 +47,34 @@ module.exports = {
           limit: 250000,
         },
       },
-      {
-        test: /materialize-css\/bin\//,
-        loader: 'imports?jQuery=jquery,$=jquery,hammerjs',
-      },
     ],
   },
   resolve: {
-    extensions: ['.jsx', '.js'],
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './server/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new TransferWebpackPlugin([{ from: 'client/images', to: 'images' }]),
+  ],
+  devServer: {
+    historyApiFallback: true,
   },
 };
